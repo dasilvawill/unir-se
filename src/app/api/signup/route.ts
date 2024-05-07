b7bd3d548c485a4f1cf2738a78ebe12c33caab23
Jaimerson Cabral <jaimersoncabral@gmail.com>
2024-05-07 16:32:27 +0000

    `signup` api filds validation

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,4 +1,13 @@
+import { userSchema } from "@/app/api/signup/schema";
+
 export async function POST(request: Request) {
-  const data = await request.json();
-  return Response.json(data);
+  try {
+    const data = await request.json();
+    const { name, email, password, passwordConfirmation } =
+      userSchema.parse(data);
+    console.log(name, email, password, passwordConfirmation);
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error: error }, { status: 400 });
+  }
 }
---
+++ src/app/api/signup/schema.ts
@@ -0,0 +1,48 @@
+import { z } from "zod";
+
+export const userSchema = z.object({
+  name: z
+    .string({
+      required_error: "O campo nome é obrigatório.",
+    })
+    .min(3, { message: "O nome deve ter mais de 3 caracteres." })
+    .max(255, { message: "O campo nome não deve ter mais de 255 caracteres." })
+    .refine(
+      (name) => {
+        const parts = name.trim().split(" ");
+        return parts.length >= 2 && parts.every((part) => part.length > 0);
+      },
+      { message: "Informe um nome e um sobrenome." }
+    ),
+
+  email: z
+    .string({
+      required_error: "O campo e-mail é obrigatório.",
+    })
+    .max(255, {
+      message: "O campo e-mail não deve ter mais de 255 caracteres.",
+    })
+    .email({ message: "Informe um e-mail válido." })
+    .transform((email) => email.trim().toLocaleLowerCase()),
+
+  password: z
+    .string({
+      required_error: "O campo senha é obrigatório.",
+    })
+    .min(8, { message: "O campo senha deve ter mais de 8 caracteres." })
+    .max(255, {
+      message: "O campo senha não deve ter mais de 255 caracteres.",
+    }),
+
+  passwordConfirmation: z
+    .string({
+      required_error: "O campo confirmação de senha é obrigatório.",
+    })
+    .min(8, {
+      message: "O campo confirmação de senha deve ter mais de 8 caracteres.",
+    })
+    .max(255, {
+      message:
+        "O campo confirmação de senha não deve ter mais de 255 caracteres.",
+    }),
+});
