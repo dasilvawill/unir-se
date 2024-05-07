2f22da0cff76a34d3abedec6b43627f0cf2ef85f
Jaimerson Cabral <jaimersoncabral@gmail.com>
2024-05-07 18:12:41 +0000

    add prisma

---
+++ prisma/migrations/20240507172055_initial_migrations_of_users_model/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,4 +1,5 @@
 import { userSchema } from "@/app/api/signup/schema";
+import prisma from "@/app/database/prismaClient";
 
 export async function POST(request: Request) {
   try {
@@ -6,8 +7,22 @@ export async function POST(request: Request) {
     const { name, email, password, passwordConfirmation } =
       userSchema.parse(data);
     console.log(name, email, password, passwordConfirmation);
-    return Response.json(data);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+    return Response.json(user);
   } catch (error) {
     return Response.json({ error: error }, { status: 400 });
   }
+}
+
+/* metodo temporário */
+export async function GET(request: Request) {
+  const users = await prisma.user.findMany();
+  return Response.json(users);
 }
