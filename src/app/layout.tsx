180559401d0d15f2641d8da565d1f3e784f1b8d4
Jaimerson Cabral <jaimersoncabral@gmail.com>
2024-05-09 13:42:21 +0000

    change layout language

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={inter.className}>{children}</body>
     </html>
   );
