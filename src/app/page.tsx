0c02a2a6f6b70189f395490b186bb931496c3a4f
William <39096909+dasilvawill@users.noreply.github.com>
2024-05-10 02:29:54 +0000

    fix redirect entrar mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -102,7 +102,7 @@ export default function Home() {
                 </div>
                 <div className="py-6">
                   <LinkComponent
-                    href="#"
+                    href="/signin"
                     text="Entrar"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                     spanContent="→"
