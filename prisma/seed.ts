f6b7009fbc0b2d878caa673bcc5264df3f7aff9d
William <39096909+dasilvawill@users.noreply.github.com>
2024-05-14 02:49:02 +0000

    validate seed duplication

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,18 +6,48 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+  const rolesExists = await prisma.role.findMany();
+
+  const rolesToCreate = roles.filter(
+    (role) =>
+      !rolesExists.find(
+        (existingRole) => existingRole.description === role.description,
+      ),
+  );
+
   const createRolesPromise = prisma.role.createMany({
-    data: roles,
+    data: rolesToCreate,
   });
 
+  const permissionsExists = await prisma.permission.findMany();
+
+  const permissionsToCreate = permissions.filter(
+    (permission) =>
+      !permissionsExists.find(
+        (existingPermission) =>
+          existingPermission.description === permission.description,
+      ),
+  );
+
   const createPermissionsPromise = prisma.permission.createMany({
-    data: permissions,
+    data: permissionsToCreate,
   });
 
   await Promise.all([createRolesPromise, createPermissionsPromise]);
 
+  const rolePermissionsExists = await prisma.rolePermission.findMany();
+
+  const rolePermissionsToCreate = rolePermissions.filter((rolePermission) => {
+    return !rolePermissionsExists.find((existingRolePermission) => {
+      return (
+        existingRolePermission.role_id === rolePermission.role_id &&
+        existingRolePermission.permission_id === rolePermission.permission_id
+      );
+    });
+  });
+
   await prisma.rolePermission.createMany({
-    data: rolePermissions,
+    data: rolePermissionsToCreate,
   });
 }
 
